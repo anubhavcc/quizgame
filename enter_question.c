e7e500ecc5dad158ca3a5a19ae479206d7d6d6ca
Anubhav Choudhary <456anubhav@gmail.com>
2017-06-13 18:20:28 +0530

    program to enter the quesstions into binary file

--- enter_question.c
+++ enter_question.c
@@ -1,3 +1,5 @@
+/*This program is written to insert quiz questions into a binary file*/
+
 #include <stdio.>
 #include "func.h"
 
@@ -7,7 +9,7 @@ main()
     int counter;
     FILE *ptr;
     struct quiz_format record;
-    ptr=fopen("test.bin",wb);
+    ptr=fopen("test.bin","wb");
     
     if (!ptr) {
         printf("Unable to open file\n");
@@ -15,18 +17,18 @@ main()
     }
     for(counter=1; counter<=20;counter++) {
         printf("Enter the question no\n");
-        scanf("%d",&record.no);
-        printf("Enter the question\n";
+        scanf("%d",record.question_no);
+        printf("Enter the question\n");
         scanf("%s",record.question);
-        printf("Enter the option 1\n";
+        printf("Enter the option 1\n");
         scanf("%s",record.option_1);
-        printf("Enter the option 2\n";
+        printf("Enter the option 2\n");
         scanf("%s",record.option_2);
-        printf("Enter the option 3\n";
+        printf("Enter the option 3\n");
         scanf("%s",record.option_3);
-        printf("Enter the option 4\n";
+        printf("Enter the option 4\n");
         scanf("%s",record.option_4);
-        printf("Enter the question answer\n";
+        printf("Enter the question answer\n)";
         scanf("%s",record.question_answer);
         fwrite(&record,sizeof(struct quiz_format),1,ptr);
     }
