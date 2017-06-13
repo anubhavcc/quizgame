d542eaa9e9cafdfce5357bce13937643805be8b6
Anubhav Choudhary <456anubhav@gmail.com>
2017-06-13 10:17:11 +0530

    structure definition

--- func.h
+++ func.h
@@ -4,11 +4,11 @@
 struct quiz_format {
     
     int  question_no;
-    char question[20];
-    char option_1;
-    char option_2;
-    char option_3;
-    char option_4;
+    char question[40];
+    char option_1[20];
+    char option_2[20];
+    char option_3[20];
+    char option_4[20];
     char question_answer;
 
 };
