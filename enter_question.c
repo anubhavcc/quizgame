14a65282147be66ab3b9f44fa636bba3b69b28fc
Anubhav Choudhary <456anubhav@gmail.com>
2017-06-13 19:12:48 +0530

    Create enter_question.c

--- enter_question.c
+++ enter_question.c
@@ -9,28 +9,28 @@ main()
     int counter;
     FILE *ptr;
     struct quiz_format record;
-    ptr=fopen("test.bin","wb");
+    ptr=fopen("test.bin", "wb");
     
     if (!ptr) {
         printf("Unable to open file\n");
         return 1;
     }
-    for(counter=1; counter<=20;counter++) {
+    for(counter=1; counter<=20; counter++) {
         printf("Enter the question no\n");
-        scanf("%d",record.question_no);
+        scanf("%d", record.question_no);
         printf("Enter the question\n");
-        scanf("%s",record.question);
+        scanf("%s", record.question);
         printf("Enter the option 1\n");
-        scanf("%s",record.option_1);
+        scanf("%s", record.option_1);
         printf("Enter the option 2\n");
-        scanf("%s",record.option_2);
+        scanf("%s", record.option_2);
         printf("Enter the option 3\n");
-        scanf("%s",record.option_3);
+        scanf("%s", record.option_3);
         printf("Enter the option 4\n");
-        scanf("%s",record.option_4);
+        scanf("%s", record.option_4);
         printf("Enter the question answer\n)";
-        scanf("%s",record.question_answer);
-        fwrite(&record,sizeof(struct quiz_format),1,ptr);
+        scanf("%s", record.question_answer);
+        fwrite(&record, sizeof(struct quiz_format), 1, ptr);
     }
     fclose(ptr);
     return 0;
