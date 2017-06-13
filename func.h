7398673c5abeff769be8901e55dde505d42abf4d
Anubhav Choudhary <456anubhav@gmail.com>
2017-06-13 10:13:18 +0530

    contains declaration of structure

---
+++ func.h
@@ -0,0 +1,18 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+struct quiz_format {
+    
+    int  question_no;
+    char question[20];
+    char option_1;
+    char option_2;
+    char option_3;
+    char option_4;
+    char question_answer;
+
+};
+
+
+
+#endif
