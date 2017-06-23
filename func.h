30537812f00770ee8c4148f507356a846eb5db1d
Anubhav Choudhary <456anubhav@gmail.com>
2017-06-23 19:18:53 +0530

    removing question no variable

--- func.h
+++ func.h
@@ -3,13 +3,12 @@
 
 struct quiz_format {
     
-    int  question_no;
     char question[40];
     char option_1[20];
     char option_2[20];
     char option_3[20];
     char option_4[20];
-    char question_answer;
+    int question_answer;
 
 };
 
