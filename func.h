ec2cd3b85d51736d21e74ec4e05fd08e56f9c5bf
Anubhav Choudhary <456anubhav@gmail.com>
2017-07-09 10:40:59 +0530

    adding  character variable option

--- func.h
+++ func.h
@@ -1,17 +1,31 @@
+
 #ifndef FUNC_H
 #define FUNC_H
-
+#include <curses.h>
+#include <menu.h>
 struct quiz_format {
     
-    char question[40];
-    char option_1[20];
-    char option_2[20];
-    char option_3[20];
-    char option_4[20];
-    int question_answer;
+    char question[60];
+    char option_blank[60];
+    char option_1[60];
+    char option_2[60];
+    char option_3[60];
+    char option_4[60];
+    char question_answer[2];
 
 };
 
+char *option[] =  {
 
+                    "1.","2.",
+                    "3.","4.",
+                    (char *)NULL,
+
+                   };
+
+int display_options(WINDOW *menu_window, struct quiz_format detail);
+int display_question(WINDOW *question_window, int count, struct quiz_format detail);
 
 #endif
+
+
