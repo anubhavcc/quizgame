5e909440bc86070e499cd38796f4473cad6c2cbd
Anubhav Choudhary <456anubhav@gmail.com>
2017-07-09 10:46:03 +0530

    contains executables

---
+++ quiz/CMakeFiles/question.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/question.dir/enter_question.c.o"
+  "question.pdb"
+  "question"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/question.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
