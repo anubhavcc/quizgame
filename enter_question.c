50ab598945f7d7d91e8a1e029245736a9d3e6ec4
Anubhav Choudhary <456anubhav@gmail.com>
2017-07-09 10:38:40 +0530

    removing question no

--- enter_question.c
+++ enter_question.c
@@ -5,29 +5,27 @@ int
 main()
  {
 
- 
-     int counter=0,noOfQuestions;size_t len;
+     int counter=0,noOfQuestions;
+     size_t len;
      FILE *ptr;
      struct quiz_format record;
-     ptr=fopen("test.bin", "wb");
+     ptr=fopen("tes.bin", "wb");
      
      if (!ptr) {
          printf("Unable to open file\n");
          return 1;
      }
      printf("Enter the number of questions\n");
-	 scanf("%d",&noOfQuestions);
+     scanf("%d",&noOfQuestions);
 
      while(counter != noOfQuestions) {
-     printf("Enter the question no\n");
-    //scanf("%d", &record.question_no);
      getchar();
-   
      printf("Enter the question\n");
      fgets(record.question,100,stdin);
-     
-     printf("Enter the option 1\n");
-     fgets(record.option_1,100,stdin);
+     printf("Enter the option blank\n");
+     fgets(record.option_blank,100,stdin);
+     printf("Enter option 1\n");
+     fgets(record.option_1, 100, stdin);
      printf("Enter the option 2\n");
      fgets(record.option_2,100,stdin);
      printf("Enter the option 3\n");
@@ -35,7 +33,7 @@ main()
      printf("Enter the option 4\n");
      fgets(record.option_4,100,stdin);
      printf("Enter the question answer\n");
-     scanf("%d",&record.question_answer);
+     scanf("%s",record.question_answer);
      fwrite(&record, sizeof(struct quiz_format), 1, ptr);
      counter++;
      }
